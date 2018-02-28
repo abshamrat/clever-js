de528da1ddb971de880dbc53917affa54564e320
shamrat17 <shamratakbar17@gmail.com>
2018-02-28 23:18:00 +0600

    modified

--- demo.php
+++ demo.php
@@ -1,5 +1,13 @@
 <?php 
-	
+
+	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+	{
+
+		echo "from ajax";
+
+	}
+
 	echo "hello";
+
 
  ?>
